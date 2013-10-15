e7c0da1e93ada37f86249cb4934f4ca20050db48
Vladimir Ivic <vlada.ivic@gmail.com>
2013-10-15 08:19:56 -0700

    Update perceptron.rb

--- lib/intellect/perceptron.rb
+++ lib/intellect/perceptron.rb
@@ -3,22 +3,25 @@ module Intellect
     attr_reader :weights, :activation, :learning_rate
     
     def initialize(layers = [], activaton = nil)
-      @activation ||= lambda { |x| 1/(1+Math.exp(-1*(x))) }
-      @weights = generate_weights(layers.size)
+      @activation ||= lambda { |x| 1 / (1 + Math.exp(-1 * x)) }
+      @weights = generate_weights(layers)
     end
     
+    # Belongs to Network
     def train(input, result)
     end
     
+    # Belongs to Network
     def eval(input)
     end
     
-    def feedforward(input) 
+    def feedforward(input)
+      activation.call(input)
     end
     
     private
     
-    def generate_weights(size)
+    def generate_weights(layers)
       @weights = layers.inject([]) do |layer, nodes|
         layer << nodes.times.inject([]) { |s, n| s << rand }
       end
