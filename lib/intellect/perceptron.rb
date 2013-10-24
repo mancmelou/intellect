d5f29ace669b1333a65b501739fb0bea907ae188
Vladimir Ivic <vlada.ivic@gmail.com>
2013-10-24 16:25:03 -0700

    Update perceptron.rb

--- lib/intellect/perceptron.rb
+++ lib/intellect/perceptron.rb
@@ -1,30 +1,46 @@
 module Intellect
-  class Perceptron
-    attr_reader :weights, :activation, :learning_rate
+  class Network
+    attr_reader :network
     
-    def initialize(layers = [], activaton = nil)
-      @activation ||= lambda { |x| 1 / (1 + Math.exp(-1 * x)) }
-      @weights = generate_weights(layers)
-    end
-    
-    # Belongs to Network
-    def train(input, result)
-    end
-    
-    # Belongs to Network
-    def eval(input)
-    end
-    
-    def feedforward(input)
-      activation.call(input)
+    def initialize(layers = [], opts = {})
+      @network = []
+      
+      layers.each do |n|
+        @network << n.times.reduce([]) { |sum, i| sum + Perceptron.new(i) }
+      end
     end
     
     private
     
-    def generate_weights(layers)
+    def generate_weights!(layers)
       @weights = layers.inject([]) do |layer, nodes|
         layer << nodes.times.inject([]) { |s, n| s << rand }
       end
+    end
+  end
+  
+  class Perceptron
+    attr_reader :weights, :activate
+    
+    def initialize(n_inputs, activation = nil)
+      @activate = activation || lambda { |x| 1 / (1 + Math.exp(-1 * x)) }
+      generate_weights(n_inputs)
+    end
+    
+    def feedforward(input = [])
+      sum = 0
+      
+      input.each_with_index do |n, i|
+        sum += n * weights[i]
+      end
+      
+      activate.call(sum)
+    end
+    
+    private
+    
+    def generate_weights(n_inputs)
+      n_inputs.times.reduce([]) { |sum, n| sum << rand }
     end
   end
 end
